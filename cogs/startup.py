c1909ccbadc16f03f920a973deb021cddea0b2c9
joonyoungpark19 <54904921+joonyoungpark19@users.noreply.github.com>
2022-01-10 15:56:57 -0500

    update

--- cogs/startup.py
+++ cogs/startup.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 
-class Example(commands.Cog):
+class Startup(commands.Cog):
 
     def __init__(self, client):
         self.client = client
@@ -18,4 +18,4 @@ class Example(commands.Cog):
 
 
 def setup(client):
-    client.add_cog(Example(client))
+    client.add_cog(Startup(client))
