bf0b43743c12d55412123aea6166a6e55a24315b
joonyoungpark19 <54904921+joonyoungpark19@users.noreply.github.com>
2022-01-15 16:34:15 -0500

    localized the data

--- cogs/scanner.py
+++ cogs/scanner.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 
-apex_counter = 0  # global variable
 scanlist = ['apex legends', 'apex']
 
 
@@ -15,13 +14,21 @@ class Scanner(commands.Cog):
         for x in scanlist:
             if x in payload.content.lower():
                 # print("Found!")
-                global apex_counter
-                apex_counter += 1
-                print(scanlist)
-                if apex_counter > 1:
-                    await payload.channel.send(":dog::poop: has been mentioned " + str(apex_counter) + " times")
+                data_channel = self.client.get_channel(932013482486947951)
+
+                # await data_channel.send("**__Apex Counter:__**")
+                # await data_channel.send("0")
+
+                apex_data = await data_channel.fetch_message(932024492295872522)
+                print(apex_data.content)
+                counter = int(apex_data.content) + 1     # add one to the count and store as new var
+                await apex_data.edit(content=str(counter))
+                # print(scanlist)
+
+                if counter > 1:
+                    await payload.channel.send(":dog::poop: has been mentioned " + str(counter) + " times")
                 else:
-                    await payload.channel.send(":dog::poop: has been mentioned " + str(apex_counter) + " time")
+                    await payload.channel.send(":dog::poop: has been mentioned " + str(counter) + " time")
             # print(payload.content)
 
 
