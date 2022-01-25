8d20c0902b4dfa2eb8668b8608851f50386ffa0c
joonyoungparkdev <54904921+joonyoungparkdev@users.noreply.github.com>
2022-01-25 13:59:04 -0500

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -39,4 +39,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('OTI5NTA2NjE4MjQzMTgyNzAy.YdoUfw._d5131t94OvDoNvZrJkM-WSxcW4')
+
